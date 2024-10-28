50a68062114aa595887bcf7caa5d98a1fdc552f2
elecun <elecun2@gmail.com>
2024-10-28 10:28:09 +0900

    add getting video info

---
+++ util/analyzer/showinfo.py
@@ -0,0 +1,56 @@
+'''
+Show AVI Information
+@auhtor Byunghun Hwang<bh.hwnag@iae.re.kr>
+'''
+
+import sys, os
+import pathlib
+import json
+from datetime import datetime
+import argparse
+import time
+import cv2
+
+# root directory registration on system environment
+ROOT_PATH = pathlib.Path(__file__).parent
+PROJECT_NAME = pathlib.Path(__file__).parent.stem
+sys.path.append(ROOT_PATH.as_posix())
+
+from avsim_monitor.window import AppWindow
+from util.logger.console import ConsoleLogger
+
+
+if __name__ == "__main__":
+
+    console = ConsoleLogger.get_logger()
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', nargs='?', required=True, help="Video File(*.avi)")
+    args = parser.parse_args()
+    
+    try:
+
+        target_file = pathlib.Path(args.file)
+        console.info(f"Target File : {target_file.as_posix()}")
+
+        target = cv2.VideoCapture(target_file.as_posix()) # load target video file
+        if not target.isOpened():
+            console.error(f"Could not open video file.")
+        else:
+            total_frames = int(target.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = target.get(cv2.CAP_PROP_FPS)
+            
+            print("================================")
+            print(f"File : {target_file.as_posix()}")
+            print(f"Total Frames : {total_frames}")
+            print(f"FPS : {fps}")
+            print(f"Duration : {total_frames/fps}")
+            print("================================")
+
+            target.release()
+        
+
+
+    except Exception as e:
+        console.critical(f"{e}")
