30eda70ab3e6e7c09fb06f16a87815317a1d03b5
elecun <elecun2@gmail.com>
2024-12-10 23:48:26 +0900

    fixed

--- webview/cabinview/app_cabinview/urls.py
+++ webview/cabinview/app_cabinview/urls.py
@@ -4,6 +4,6 @@ from app_cabinview import views
 
 urlpatterns = [
     path('', views.index, name="cabinview_index"),
-    path('event/', views.button_event, name="cabinview_event"),
+    path('event/', views.event, name="cabinview_event"),
     path('wifi/', views.wifi_qr, name="cabinview_wifi_qr"),
 ]
--- webview/cabinview/app_cabinview/views.py
+++ webview/cabinview/app_cabinview/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import redirect
 def index(request):
     return render(request, "index.html")
 
-def button_event(request):
-    return render(request, "button_event.html")
+def event(request):
+    return render(request, "event.html")
 
 def wifi_qr(request):
     return render(request, "wifi_qr.html")
